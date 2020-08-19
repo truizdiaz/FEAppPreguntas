101150caec69dc98fc8b813411af4392eb579734
truizdiaz <tomiruizdiaz@gmail.com>
2020-08-19 15:19:34 -0300

    Estrucutra de carpetas + creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './componets/inicio/inicio.component';
+import { BienvenidaComponent } from './componets/inicio/bienvenida/bienvenida.component';
+import { LoginComponent } from './componets/inicio/login/login.component';
+import { RegisterComponent } from './componets/inicio/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    BienvenidaComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
