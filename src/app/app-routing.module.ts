f876313da4b010e83e633a88911df4fe1291dfe6
truizdiaz <tomiruizdiaz@gmail.com>
2020-08-20 10:38:33 -0300

    Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BienvenidaComponent } from './componets/inicio/bienvenida/bienvenida.component';
+import { RegisterComponent } from './componets/inicio/register/register.component';
+import { LoginComponent } from './componets/inicio/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/bienvenidos', pathMatch: 'full' },
+  { path: 'bienvenidos', component: BienvenidaComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: '/bienvenidos', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
