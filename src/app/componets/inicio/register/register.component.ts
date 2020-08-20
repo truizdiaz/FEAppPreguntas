6977dbc8652d5ace5e775610264dd72374257ca3
truizdiaz <tomiruizdiaz@gmail.com>
2020-08-20 16:09:58 -0300

    RegisterComponet HTML

--- src/app/componets/inicio/register/register.component.ts
+++ src/app/componets/inicio/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
+  register: FormGroup;
   constructor() { }
 
   ngOnInit(): void {
