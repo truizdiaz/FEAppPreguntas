4d05730264d43b1286c644494a07774774433b75
truizdiaz <tomiruizdiaz@gmail.com>
2020-08-20 12:12:39 -0300

    LoginComponent enlazando HTML Y Componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Modulos
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// Componentes
 import { AppComponent } from './app.component';
 import { InicioComponent } from './componets/inicio/inicio.component';
 import { BienvenidaComponent } from './componets/inicio/bienvenida/bienvenida.component';
@@ -18,7 +22,8 @@ import { RegisterComponent } from './componets/inicio/register/register.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/componets/inicio/login/login.component.ts
+++ src/app/componets/inicio/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  login: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.login = this.fb.group({
+      usuario: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  log(): void{
+    console.log(this.login);
   }
 
 }
