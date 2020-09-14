9f8c65b735808840a261737cf864b62e4664dffc
truizdiaz <tomiruizdiaz@gmail.com>
2020-09-14 10:47:03 -0300

    5S 2M SaveUser

--- src/app/components/inicio/register/register.component.ts
+++ src/app/components/inicio/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Usuario } from 'src/app/models/usuario';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-register',
@@ -9,7 +11,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class RegisterComponent implements OnInit {
   register: FormGroup;
 
-  constructor(private fb: FormBuilder) { 
+  constructor(private fb: FormBuilder, private usuarioService: UsuarioService) { 
     this.register = this.fb.group({
       usuario: ['', Validators.required],
       password: ['', [Validators.required, Validators.minLength(4)]],
@@ -22,6 +24,14 @@ export class RegisterComponent implements OnInit {
 
   registrarUsuario(): void{
     console.log(this.register);
+
+    const usuario: Usuario ={
+      nombreUsuario: this.register.value.usuario,
+      password: this.register.value.password
+    }
+    this.usuarioService.saveUser(usuario).subscribe(data => {
+      console.log(data);
+    });
   }
 
   checkPassword(group: FormGroup): any {
--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Usuario } from '../models/usuario';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,8 +15,8 @@ export class UsuarioService {
     this.myAppUrl = environment.endpoint;
     this.myApiUrl = '/api/Usuario';
    }
-
-  saveUser(usuario: Usuario): void{
-
+ // http://localhost:50451/api/Usuario -- POST
+  saveUser(usuario: Usuario): Observable<any>{
+    return this.http.post(this.myAppUrl + this.myApiUrl, usuario);
   }
 }
