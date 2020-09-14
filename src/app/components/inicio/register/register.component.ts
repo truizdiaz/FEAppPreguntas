4facc9fe012f6fe56292a4e7b827c4f39211a5a6
truizdiaz <tomiruizdiaz@gmail.com>
2020-09-14 12:52:02 -0300

    SaveUser Parte2 Terminado

--- src/app/components/inicio/register/register.component.ts
+++ src/app/components/inicio/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Usuario } from 'src/app/models/usuario';
 import { UsuarioService } from 'src/app/services/usuario.service';
 
@@ -10,8 +12,12 @@ import { UsuarioService } from 'src/app/services/usuario.service';
 })
 export class RegisterComponent implements OnInit {
   register: FormGroup;
+  loading = false;
 
-  constructor(private fb: FormBuilder, private usuarioService: UsuarioService) { 
+  constructor(private fb: FormBuilder, 
+              private usuarioService: UsuarioService,
+              private router: Router,
+              private toastr: ToastrService) { 
     this.register = this.fb.group({
       usuario: ['', Validators.required],
       password: ['', [Validators.required, Validators.minLength(4)]],
@@ -28,9 +34,19 @@ export class RegisterComponent implements OnInit {
     const usuario: Usuario ={
       nombreUsuario: this.register.value.usuario,
       password: this.register.value.password
-    }
+    };
+    this.loading = true;
     this.usuarioService.saveUser(usuario).subscribe(data => {
       console.log(data);
+      this.toastr.success('El usuario ' + usuario.nombreUsuario + ' fue registrado con exito!', 'Usuario Registrado!');
+      this.router.navigate(['/inicio/login']);
+      this.loading = false;
+    }, error => {
+      this.loading = false;
+      console.log(error);
+      this.toastr.error(error.error.message, 'Error!');
+      this.register.reset();
+
     });
   }
 
