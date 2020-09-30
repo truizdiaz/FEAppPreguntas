7f9163058bdf7992e4acd391480d3482c219a24f
truizdiaz <tomiruizdiaz@gmail.com>
2020-09-30 17:24:12 -0300

    Mostrar/Ocular detalle pregunta

--- src/app/models/pregunta.ts
+++ src/app/models/pregunta.ts
@@ -8,5 +8,6 @@ export class Pregunta{
     constructor(descripcion: string, respuestas: Respuesta[]){
         this.descripcion = descripcion;
         this.listRespuestas = respuestas;
+        this.hide = true;
     }
 }
