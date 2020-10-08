6ef0e01e8ea3c59866ce2340c480b5d4a7553512
truizdiaz <tomiruizdiaz@gmail.com>
2020-10-08 09:05:15 -0300

    Iterando respuestas

--- src/app/components/inicio/list-cuestionarios/pregunta/pregunta.component.ts
+++ src/app/components/inicio/list-cuestionarios/pregunta/pregunta.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Pregunta } from 'src/app/models/pregunta';
 import { CuestionarioService } from 'src/app/services/cuestionario.service';
 import { RespuestaCuestionarioService } from 'src/app/services/respuesta-cuestionario.service';
 
@@ -10,6 +11,7 @@ import { RespuestaCuestionarioService } from 'src/app/services/respuesta-cuestio
 })
 export class PreguntaComponent implements OnInit {
   idCuestionario: number;
+  listPreguntas: Pregunta[] = [];
 
   constructor(private respuestaCuestionarioService: RespuestaCuestionarioService,
               private cuestionarioService: CuestionarioService,
@@ -27,7 +29,18 @@ export class PreguntaComponent implements OnInit {
   getCuestionario(): void {
     this.cuestionarioService.getCuestionario(this.idCuestionario).subscribe(data =>{
       console.log(data);
+      this.listPreguntas = data.listPreguntas;
     });
   }
+
+  obtenerPregunta(): string {
+    return this.listPreguntas[0].descripcion;
+  }
+
+  getIndex(): number {
+    return 0;
+  }
+
+
 
 }
