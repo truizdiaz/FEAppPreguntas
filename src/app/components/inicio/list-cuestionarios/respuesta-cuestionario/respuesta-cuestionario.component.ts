476657e25243cee027954f32a4c5da29c62d67f3
truizdiaz <tomiruizdiaz@gmail.com>
2020-10-08 14:01:56 -0300

    Respuesta cuestionario

--- src/app/components/inicio/list-cuestionarios/respuesta-cuestionario/respuesta-cuestionario.component.ts
+++ src/app/components/inicio/list-cuestionarios/respuesta-cuestionario/respuesta-cuestionario.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Cuestionario } from 'src/app/models/cuestionario';
+import { RespuestaCuestionarioService } from 'src/app/services/respuesta-cuestionario.service';
 
 @Component({
   selector: 'app-respuesta-cuestionario',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./respuesta-cuestionario.component.css']
 })
 export class RespuestaCuestionarioComponent implements OnInit {
+  cuestionario: Cuestionario;
+  respuestaUsuario: number[] = [];
 
-  constructor() { }
+  constructor(private respuestaCuestionarioService: RespuestaCuestionarioService,
+              private router: Router) { }
 
   ngOnInit(): void {
+    if (this.respuestaCuestionarioService.idCuestionario == null) {
+      this.router.navigate(['/inicio']);
+    } else {
+      this.cuestionario = this.respuestaCuestionarioService.cuestionario;
+      this.respuestaUsuario = this.respuestaCuestionarioService.respuestas;
+      console.log(this.cuestionario);
+      console.log(this.respuestaUsuario);
+    }
   }
 
 }
