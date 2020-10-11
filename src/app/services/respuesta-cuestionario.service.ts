94c6731528db2b2781643ff022808a337f58dbf4
truizdiaz <tomiruizdiaz@gmail.com>
2020-10-11 14:30:31 -0300

    RespuestaCuestionario Parte1

--- src/app/services/respuesta-cuestionario.service.ts
+++ src/app/services/respuesta-cuestionario.service.ts
@@ -1,14 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Cuestionario } from '../models/cuestionario';
+import { RespuestaCuestionario } from '../models/respuestaCuestionario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RespuestaCuestionarioService {
+  myAppUrl: string;
+  myApiUrl: string;
+
   nombreParticiante: string;
   idCuestionario: number;
   respuestas: number[] = [];
   cuestionario: Cuestionario;
-  
-  constructor() { }
+
+  constructor(private http: HttpClient) {
+    this.myAppUrl = environment.endpoint;
+    this.myApiUrl = '/api/RespuestaCuestionario/';
+  }
+
+  guardarRespuestaCuestionario(respuestaCuestionario: RespuestaCuestionario): Observable<any> {
+    return this.http.post(this.myAppUrl + this.myApiUrl, respuestaCuestionario)
+  }
 }
